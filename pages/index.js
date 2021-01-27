a645a73de559b4ac0d15e2cadeae01f89c9f29fe
kauan1 <k219594@dac.unicamp.br>
2021-01-27 13:14:34 -0300

    aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import db from "../db.json";
-import Widget from "../src/components/Widget";
-import Footer from "../src/components/Footer";
-import GitHubCorner from "../src/components/GitHubCorner";
-import QuizBackground from "../src/components/QuizBackground";
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -16,28 +18,45 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
   return (
-    <QuizBackground backgroundImage={db.bg}> 
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>League of Legends</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>sdfghjk bhjknsdn snskn</p>
+            <form onSubmit={(event) => {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              // router manda para a proxima pagina
+            }}
+            >
+              <input
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+                placeholder="Diz ai seu nome para jogar :)"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
-        <Widget.Content>
+          <Widget.Content>
             <h1>Todos os mains são aceitos</h1>
             <p>sdfghjk bhjknsdn snskn</p>
           </Widget.Content>
         </Widget>
-        <Footer/>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/kauan1" /> 
+      <GitHubCorner projectUrl="https://github.com/kauan1" />
     </QuizBackground>
-  )
+  );
 }
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
- const Widget = styled.div`
+const Widget = styled.div`
   margin-top: 24px;
   margin-bottom: 24px;
-  border: 1px solid ${({theme})=> theme.colors.primary};
-  background-color: ${({theme})=> theme.colors.mainBg};
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.mainBg};
   border-radius: 4px;
   overflow: hidden;
 
@@ -27,14 +27,14 @@ Widget.Header = styled.header`
   justify-content: flex-start;
   align-items: center;
   padding: 18px 32px;
-  background-color: ${({theme})=> theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.primary};
 
   *{
     margin: 0;
   }
 `;
 
- Widget.Content = styled.div`
+Widget.Content = styled.div`
   padding: 24px 32px 32px 32px;
   & > *:first-child{
     margin-top:0;
